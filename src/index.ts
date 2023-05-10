ef93ecf86ac8eee38f73bd67af74ef2650d2e210
dingjunjie <864546065@qq.com>
2023-05-10 14:55:56 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import useReflare from "reflare";
 const handleRequest = async (request: Request): Promise<Response> => {
   const reflare = await useReflare();
 
+
   reflare.push({
     path: "/*",
     upstream: {
-      domain: "chat.forefront.ai",
+      domain: "forefront.ai",
       protocol: "https",
     },
+    subdomain: true,
   });
 
   return reflare.handle(request);
